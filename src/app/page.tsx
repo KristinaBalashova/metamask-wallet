de20e8ebaa35d5b041f96a793fde54687945a508
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-03-15 14:23:15 +0100

    Set up starting repo settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,56 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+import detectEthereumProvider from "@metamask/detect-provider";
+import { MetaMaskInpageProvider } from "@metamask/providers";
+
+declare global {
+  interface Window{
+    ethereum?:MetaMaskInpageProvider
+  }
+}
 
 export default function Home() {
+
+  const [hasProvider, setHasProvider] = useState<boolean | null>(null);
+    const initialState = { accounts: [] };              /* New */
+    const [wallet, setWallet] = useState(initialState); /* New */
+
+    useEffect(() => {
+        const getProvider = async () => {
+            const provider = await detectEthereumProvider({ silent: true });
+            setHasProvider(Boolean(provider));
+        };
+
+        getProvider();
+    }, []);
+
+    const updateWallet = async (accounts: any) => {     /* New */
+        setWallet({ accounts });                        /* New */
+    };                                                  /* New */
+
+    const handleConnect = async () => {                 /* New */
+        let accounts = await window.ethereum.request({  /* New */
+            method: "eth_requestAccounts",              /* New */
+        });                                             /* New */
+        updateWallet(accounts);                         /* New */
+    };                                                  /* New */
+
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div>
+                Injected Provider {hasProvider ? "DOES" : "DOES NOT"} Exist
       </div>
+      {hasProvider && (                           /* Updated */
+                <button onClick={handleConnect}>Connect MetaMask</button>
+            )}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            {wallet.accounts.length > 0 && (            /* New */
+                <div>Wallet Accounts: {wallet.accounts[0]}</div>
+            )}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
