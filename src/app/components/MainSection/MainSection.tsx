a41863f2f2c2a49c707e8c139d2796ab849d7553
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-03-16 15:23:59 +0100

    Update wallet card on/of button

--- src/app/components/MainSection/MainSection.tsx
+++ src/app/components/MainSection/MainSection.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
 import WalletCard from './WalletCard';
 import Box from '@mui/material/Box';
+import styles from './MainSection.module.css';
 
 export default function MainSection() {
   return (
-    <Box component="section">
+    <Box component="section" className={styles.root}>
       <WalletCard />
     </Box>
   );
--- src/app/components/MainSection/WalletCard.tsx
+++ src/app/components/MainSection/WalletCard.tsx
@@ -20,21 +20,130 @@ const chainId = {
 };
 
 export default function WalletCard() {
+  const [hasProvider, setHasProvider] = useState<boolean | null>(null);
+  const initialState = { accounts: [], balance: '', chainId: '' };
+  const [wallet, setWallet] = useState(initialState);
+
+  const [isConnecting, setIsConnecting] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    const refreshAccounts = (accounts: any) => {
+      if (accounts.length > 0) {
+        updateWallet(accounts);
+      } else {
+        // if length 0, user is disconnected
+        setWallet(initialState);
+      }
+    };
+
+    const refreshChain = (chainId: any) => {
+      setWallet((wallet) => ({ ...wallet, chainId }));
+    };
+
+    const getProvider = async () => {
+      const provider = await detectEthereumProvider({ silent: true });
+      setHasProvider(Boolean(provider));
+
+      if (provider) {
+        const accounts = await window.ethereum?.request({
+          method: 'eth_accounts',
+        });
+        refreshAccounts(accounts);
+        window.ethereum?.on('accountsChanged', refreshAccounts);
+        window.ethereum?.on('chainChanged', refreshChain);
+      }
+    };
+
+    getProvider();
+
+    return () => {
+      window.ethereum?.removeListener('accountsChanged', refreshAccounts);
+      window.ethereum?.removeListener('chainChanged', refreshChain);
+    };
+  }, []);
+
+  const updateWallet = async (accounts: any) => {
+    const balance = formatBalance(
+      await window.ethereum!.request({
+        method: 'eth_getBalance',
+        params: [accounts[0], 'latest'],
+      }),
+    );
+    const chainId = await window.ethereum!.request({
+      method: 'eth_chainId',
+    });
+    setWallet({ accounts, balance, chainId });
+  };
+
+  const handleConnect = async () => {
+    setIsConnecting(true);
+    await window.ethereum
+      ?.request({
+        method: 'eth_requestAccounts',
+      })
+      .then((accounts: []) => {
+        setError(false);
+        updateWallet(accounts);
+      })
+      .catch((err: any) => {
+        setError(true);
+        setErrorMessage(err.message);
+      });
+    setIsConnecting(false);
+  };
+
+  const disableConnect = Boolean(wallet) && isConnecting;
+
   return (
-    <Box
-      component="div"
-      className={styles.root}
-    >
-      <Button variant="text" >
+    <Box component="div" className={styles.root}>
+      {!hasProvider && (
+        <Button
+          variant="contained"
+          disabled={disableConnect}
+          onClick={handleConnect}
+          sx={{ my: 2 }}
+        >
+          <a
+            href="https://chromewebstore.google.com/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn"
+            target="_blank"
+          >
+            Install MetaMask Extention
+          </a>
+        </Button>
+      )}
+      {window.ethereum?.isMetaMask && wallet.accounts.length < 1 && (
+        <Button variant="text" disabled={disableConnect} onClick={handleConnect} sx={{ my: 2 }}>
           Connnect MetaMask
         </Button>
-      <Typography variant="h6" component="h2">
-        Wallet address
-      </Typography>
-      <TextField id="standard-basic" label="Address" variant="standard" className={styles.input} />
-      <Button variant="outlined" className={styles.button}>
-        Send Data
-      </Button>
+      )}
+      {error && (
+        <div onClick={() => setError(false)}>
+          <Typography variant="subtitle2" component="h2" color="#FF0000">
+            <strong>Error:</strong> {errorMessage}
+          </Typography>
+        </div>
+      )}
+      {wallet.accounts.length > 0 && (
+        <Typography variant="h6" component="h2" color="#F28C28">
+          <strong>Your Wallet: </strong>
+          {wallet.accounts[0]}
+        </Typography>
+      )}
+
+      <Box sx={{ my: 2, display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
+        <Typography variant="h6" component="h2" color="#F28C28">
+          <img src="/eth-logo.svg" alt="github-logo" width="20" height="20" />
+          {wallet.balance}
+        </Typography>
+        <Typography variant="h6" component="h2" color="#F28C28">
+          <img src="/bnb-logo.svg" alt="github-logo" width="20" height="20" />
+          {wallet.balance}
+        </Typography>
+      </Box>
+      <TextField id="outlined-basic" label="Address" variant="outlined" sx={{ my: 2 }} />
+      <Button variant="outlined">Send</Button>
     </Box>
   );
 }
