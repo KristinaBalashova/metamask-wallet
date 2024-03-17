4f58aa874d31120781a54f687819ea1cf145c796
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-03-17 14:53:03 +0100

    Add send transaction component

---
+++ src/app/components/MainSection/SendTransaction.tsx
@@ -0,0 +1,39 @@
+import { Button } from '@mui/material';
+import React from 'react';
+import { FC } from 'react';
+import { MetaMaskInpageProvider } from '@metamask/providers';
+
+declare global {
+  interface Window {
+    ethereum?: MetaMaskInpageProvider;
+  }
+}
+
+interface IProps {
+  from: String;
+  to: String;
+  amount: Number;
+}
+
+export const SendTransaction: FC<IProps> = (props: IProps) => {
+  const { from, to, amount } = props;
+
+  const sendTransaction = async () => {
+    await window.ethereum?.request({
+      method: 'eth_sendTransaction',
+      params: [
+        {
+          to: to,
+          from: from,
+          amount: amount,
+        },
+      ],
+    });
+  };
+
+  return (
+    <Button variant="outlined" onClick={() => sendTransaction()} sx={{ mt: 2 }}>
+      Send
+    </Button>
+  );
+};
