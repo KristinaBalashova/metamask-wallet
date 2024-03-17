73b02f8ae97b1509bc9eb6000fd32d70e4069f61
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-03-18 00:02:39 +0100

    Testing useClient

--- src/app/components/MainSection/BalanceSwitcher.tsx
+++ src/app/components/MainSection/BalanceSwitcher.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Box, Button, ButtonGroup, Tooltip, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import { useMetaMask } from '../../../hooks/useMetaMask';
