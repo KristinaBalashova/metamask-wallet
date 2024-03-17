456a88a0f1f7b625b6bd97fa6dd6a6e4fce1a0d9
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-03-17 19:47:23 +0100

    Update wallet design

--- src/app/components/MainSection/Wallet.tsx
+++ src/app/components/MainSection/Wallet.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
 import { FC } from 'react';
+import { formatAddress } from "../../../utils";
 
 interface IProps {
   walletAddress: String;
@@ -10,19 +11,29 @@ export const Wallet: FC<IProps> = (props: IProps) => {
   const { walletAddress } = props;
 
   return (
-    <Box>
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
       <strong>Your Wallet:</strong>
       <Typography
         variant="subtitle1"
         noWrap
-        sx={{ color: '#EC5800', backgroundColor: '#ffedcc', borderRadius: 1, pl: 1, 
-        fontSize: {
+        sx={{
+          color: '#EC5800',
+          backgroundColor: '#ffedcc',
+          borderRadius: 1,
+          pl: 1,
+          fontSize: {
             md: 16,
             sm: 14,
-            xs: 12
-          },}}
+            xs: 12,
+          },
+        }}
       >
-        {walletAddress}
+        {formatAddress(walletAddress)}
       </Typography>
     </Box>
   );
